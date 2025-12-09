2c0a0c46cf23222053265461408be4a2246d807d
Manjunath S <manjunath.srinivas.rk@gmail.com>
2025-12-09 08:36:20 +0530

    feat(learn): added calling in main

--- main.go
+++ main.go
@@ -31,6 +31,16 @@ func scanName() {
 	var b = 15
 	swapValues(&a, &b)
 	addTen(&a)
+	var newBankAcc = BankAccount{accNo: "12345", balance: 3500, owner: "John Doe"}
+	var initBal = newBankAcc.GetBalance()
+	fmt.Println("The initial balance is: ", initBal)
+	newBankAcc.Deposit(200)
+	var withdrawresult1 = newBankAcc.Withdraw(50)
+	var withdrawresult2 = newBankAcc.Withdraw(50000)
+	fmt.Println("Withdraw result 1:", withdrawresult1)
+	fmt.Println("Withdraw result 2:", withdrawresult2)
+	var finalBal = newBankAcc.GetBalance()
+	fmt.Println("The final balance is: ", finalBal)
 	// strWrtiter := io.StringWriter(os.Stdout)
 	// cobra.WriteStringAndCheck(strWrtiter, name)
 }
